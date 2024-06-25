1b11e5ec1f36fa8b864e96312e153df6688912e8
francois-bonnin <francois.bonnin@ynov.com>
2024-06-25 14:45:39 +0200

    test: handle single number

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -4,3 +4,7 @@ import { add } from "./calculator";
 test("should return 0 for empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("should return the number itself for a single number string", () => {
+  expect(add("1")).toBe(1);
+});
