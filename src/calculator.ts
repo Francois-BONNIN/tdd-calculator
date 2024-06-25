8ca960fb2d5e9afc9ab589c952b11e939244a00b
francois-bonnin <francois.bonnin@ynov.com>
2024-06-25 15:24:17 +0200

    impl: disallow separator at the end

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,9 @@
 export function add(numbers: string): number {
   if (numbers === "") return 0;
+
+  if (numbers.endsWith(",") || numbers.endsWith("\n")) {
+    throw new Error("Invalid input: Trailing separator");
+  }
   const nums = numbers.split(/[\n,]/).map(Number);
   return nums.reduce((acc, num) => acc + num, 0);
 }
