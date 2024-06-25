cefa9cbeb1e1fa5b7efdd3e639eee81725e3f3c8
francois-bonnin <francois.bonnin@ynov.com>
2024-06-25 15:25:59 +0200

    test: handle different delimiters

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -24,3 +24,15 @@ test("should handle newlines as separators", () => {
 test("should throw an error if the string ends with a separator", () => {
   expect(() => add("1,2,")).toThrow("Invalid input: Trailing separator");
 });
+
+test("should handle different delimiters", () => {
+  expect(add("//;\n1;2")).toBe(3);
+  expect(add("//|\n1|2|3")).toBe(6);
+  expect(add("//sep\n2sep5")).toBe(7);
+});
+
+test("should throw an error for invalid input with different delimiters", () => {
+  expect(() => add("//|\n1|2,3")).toThrow(
+    "'|' expected but ',' found at position 3"
+  );
+});
