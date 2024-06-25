733e7a47ff75697b3431ff71fda8ef9bd18cba7e
francois-bonnin <francois.bonnin@ynov.com>
2024-06-25 15:00:32 +0200

    test: handle unknown number of arguments

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -12,3 +12,7 @@ test("should return the number itself for a single number string", () => {
 test("should return the sum of two numbers separated by a comma", () => {
   expect(add("1,2")).toBe(3);
 });
+
+test("should return sum for unknown number of arguments", () => {
+  expect(add("1,2,3,4,5")).toBe(15);
+});
